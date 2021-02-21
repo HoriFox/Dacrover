e4434f70e2070e7152ffe68f5c562459ff2a86fe
HoriFox <lhorifoxl@gmail.com>
2021-02-22 00:44:10 +0300

    Fast fix and add load default config

--- server.py
+++ server.py
@@ -6,26 +6,36 @@ from flask.logging import default_handler
 from treatment import Api
 
 class FlaskServer (Flask):
-    def __init__(self, import_name, config_file):
-        super(FlaskServer, self).__init__(import_name)
-        self.api_config = self.load_config(config_file)
-        self.handler = logging.StreamHandler(sys.stderr)
-        self.logger.removeHandler(default_handler)
-        self.logger.addHandler(self.handler)
-        self.logger.setLevel(logging.DEBUG)
-        self.api = Api(self.api_config, self.logger)
+	def __init__(self, import_name, config_file):
+		super(FlaskServer, self).__init__(import_name)
+		self.api_config = self.load_config(config_file)
+		self.handler = logging.StreamHandler(sys.stderr)
+		self.logger.removeHandler(default_handler)
+		self.logger.addHandler(self.handler)
+		self.logger.setLevel(logging.DEBUG)
+		self.api = Api(self.api_config, self.logger)
 
-    def load_config(self, path):
-        if not path:
-            path='api.config.json'
-        return load_json(path)
+	def load_config(self, path):
+		config = {"user_mysql":"smarthomeuser",
+			"password_mysql":"password",
+			"host_mysql":"127.0.0.1",
+			"port_mysql":3306,
+			"database_mysql":"smarthome"}
+		path='/etc/assol/api.config.json'
+		try:
+			with open(path) as file:
+				data = load_json(path)
+				config.update(data)
+		except:
+			eprint('[!]Cant load config from %s' % path)
+		return config
 
-    def setup_route(self):
-        self.add_url_rule('/', "run_api", self.api.run_api, methods=['POST'])
-        self.add_url_rule('/', "root", self.api.root, methods=['GET'])
-        self.add_url_rule('/data', "data_transfer_request", self.api.data_transfer_request, methods=['POST'])
+	def setup_route(self):
+		self.add_url_rule('/', "run_api", self.api.run_api, methods=['POST'])
+		self.add_url_rule('/', "root", self.api.root, methods=['GET'])
+		self.add_url_rule('/data', "data_transfer_request", self.api.data_transfer_request, methods=['POST'])
 
 def create_app(config_file):
-    app = FlaskServer('FlaskServer', config_file)
-    app.setup_route()
-    return app
+	app = FlaskServer('FlaskServer', config_file)
+	app.setup_route()
+	return app
